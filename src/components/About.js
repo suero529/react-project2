3438ce988f06bb61430179e31de11fb8b7f20f0b
Susan Choi <itsmesusanee@gmail.com>
2021-11-29 23:37:24 -0500

    dancing broccoli

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,13 @@ const About = () => {
         <p>Motivational blurb here</p>
         <p>Motivational blurb here</p>
         <p>Motivational blurb here</p>
+        <br />
+        <p>...and remember to supplement your workouts with a healthy diet!</p>
+        <img
+          className="dancing-broccoli"
+          src="https://cdn.dribbble.com/users/948184/screenshots/7798456/media/14fa865a554e86f90d499834af7f1d32.gif"
+          alt="dancing broccoli"
+        />
       </div>
       <Link className="link" to="/main">
         <button>Time to plan it out!</button>
