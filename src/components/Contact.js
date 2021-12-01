9bbb38b289fea0a16e9ecf1d4158121415590cea
Susan Choi <itsmesusanee@gmail.com>
2021-12-01 01:31:03 -0500

    contact page styling done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,37 +3,63 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h3>Contact Information</h3>
-      <p class="address">
-        Citric Circuit
-        <p>123 Avocardio St, </p>
-        <p>NYC,</p>
-        <p>US</p>
-        <p>(123) 456-7890</p>
-      </p>
-      <h3>Follow Us</h3>
-      <ul className="social-media">
-        <li>
-          <a href="http://www.twitter.com" class="footer a">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a href="http://www.facebook.com" class="footer a">
-            Facebook
-          </a>
-        </li>
-        <li>
-          <a href="http://www.instagram.com">Instagram</a>
-        </li>
-      </ul>
-      {/* <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.142047744348!2d2.3354330160472316!3d48.87456857928921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e38f817b573%3A0x48d69c30470e7aeb!2sIronhack!5e0!3m2!1sen!2ses!4v1495208746099"
-        class="map-frame"
-        frameborder="0"
-        style="border:0"
-        allowfullscreen
-      ></iframe> */}
+      <h1 className="contact-heading" style={{ textAlign: "center" }}>
+        Contact Information
+      </h1>
+      <div class="address-box">
+        <p
+          className="address"
+          style={{ textAlign: "center", fontSize: 22, color: "cornflowerblue" }}
+        >
+          Citric Circuit
+        </p>
+        <p
+          className="address"
+          style={{ textAlign: "center", fontSize: 22, color: "cornflowerblue" }}
+        >
+          123 Avocardio St,{" "}
+        </p>
+        <p
+          className="address"
+          style={{ textAlign: "center", fontSize: 22, color: "cornflowerblue" }}
+        >
+          NYC, US
+        </p>
+        <p
+          className="address"
+          style={{ textAlign: "center", fontSize: 22, color: "cornflowerblue" }}
+        >
+          (123) 456-7890
+        </p>
+      </div>
+      <div className="follow">
+        <h3 className="follow-us" style={{ textAlign: "center", fontSize: 25 }}>
+          Follow Us!
+        </h3>
+        <ul className="social-media">
+          <li>
+            <img
+              style={{ width: 50 }}
+              src="https://cdn-icons-png.flaticon.com/512/355/355974.png"
+              alt="twitter"
+            ></img>
+          </li>
+          <li>
+            <img
+              style={{ width: 50 }}
+              src="https://cdn-icons-png.flaticon.com/512/355/355975.png"
+              alt="ig"
+            ></img>
+          </li>
+          <li>
+            <img
+              style={{ width: 50 }}
+              src="https://cdn-icons-png.flaticon.com/512/355/355969.png"
+              alt="fb"
+            ></img>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
